5dd60bb7405a9a38a7e69fc14595fff6a7f5e5af
brandondelpozo <branzzel@gmail.com>
2022-02-11 23:28:39 -0500

    create tests.py

---
+++ test1.py
@@ -0,0 +1,46 @@
+"""
+Giving the following string
+sample = "aaabbbccbacdeb"
+
+Print the 3 most common characters along with their occurrence count each on a separate line.
+Sort output in descending order of occurrence count.
+
+Note: order by letter if the number repeats
+
+The sample output would be:
+b : 5
+a : 4
+c : 3
+"""
+
+"""
+Pseudocode
+declare string
+empty_string
+count = 0
+count_element = 0
+
+for each element in string
+if element1 is already in empty string
+count + 1
+if element1 is not in empty string
+add it
+after finish it go to element2
+count_element+=1
+
+"""
+
+#sample = "aaabbb"
+
+
+def count_char(sample):
+    new_string = " "
+    count = 0
+    for e in sample:
+        if e in new_string:
+            count+=1
+        else:
+            new_string+=e
+    print(new_string, count)
+
+count_char("aaaaabb")
---
+++ test2.py
@@ -0,0 +1,9 @@
+"""
+Giving two words please provide a list in alphabetical order of common characters found in both words, please avoid using nested loops
+Sample input:
+word_1 = 'maria'
+word_2 = 'marcela'
+
+ The sample output would be:
+['a', 'm', 'r']
+"""
